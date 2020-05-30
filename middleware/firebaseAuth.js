119a505a26fe6aaaa3e395f8687ef6d54955f34e
dargains <andre.dargains@gmail.com>
2020-05-31 00:22:24 +0100

    RFC: removed nuxt;

--- middleware/firebaseAuth.js
+++
@@ -1,9 +0,0 @@
-import Cookie from 'js-cookie'
-
-export default function ({ store, route, redirect }) {
-  const user = store.state.users.user || Cookie.get('access_token');
-  const blockedRoute = /\/admin\/*/g
-  if (!user && route.path.match(blockedRoute)) {
-    redirect('/')
-  }
-}
